99a5dde6d85cf93eafb86496e091da9ed43d5c44
Bryan Alejandro <alejandrofloresbryan@gmail.com>
2026-08-10 17:25:58 -0600

    Curso Java

---
+++ src/HolaMundo.java
@@ -0,0 +1,6 @@
+public class HolaMundo {
+    //LIKE SI CREES EN DIOS
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
---
+++ src/HolaMundo_Ejercicios.java
@@ -0,0 +1,28 @@
+//IMPORTA LA CLASE
+import java.util.Scanner;
+
+//ES LA ESTRUCTURA O CONTENEDOR PRINCIPAL DE MI PROGRAMA
+public class HolaMundo_Ejercicios {
+
+    //INICIA LA CLASE. ES LA PUERTA DE ENTRADA DE CUALQUIER PROGRAMA
+    public static void main (String[]args ) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        //LO MUESTRA EN LA CONSOLA Y MUESTRA MI NOMBRE
+        System.out.println("Bryan Alejandro Flores");
+
+        //UN SALUDO
+        /*hola bro como te encuentras estoy retomando programacion
+        CUIDATE AMO A LA CHICA DE LA IGLESIA
+         */
+System.out.println("hola");
+System.out.println("mundo");
+System.out.println("tengo 16 años mi color favorito es el azul y vivo en veracruz");
+System.out.println("hola mundo");
+System.out.println("soy bryan un chico muy curioso");
+System.out.println("ya quiero que salga gta 6 y el nuevo gears");
+System.out.println(":( / ;) / XD / :/");
+
+    }
+}
